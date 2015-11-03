3466e6adce952bb4d693682450bfb8bcbc42ce51
Dominik Steiner <dominik.j.steiner@gmail.com>
2015-11-03 16:44:38 -0500

    use sdk modules

--- test/settings.js
+++ test/settings.js
@@ -1,11 +1,16 @@
-test = require('../node_modules/webrtc-sipstack/test/includes/common')(require('../node_modules/webrtc-core/test/includes/common'));
+test = require('../node_modules/webrtc-sipstack/test/includes/common')(require('../node_modules/bdsft-sdk-test/lib/common'));
 describe('settings', function() {
 
   before(function(){
     core = require('webrtc-core');
-    test.createCore('cookieconfig');
+    test.createModelAndView('core', {
+      core: require('webrtc-core')
+    }, 'cookieconfig');
     test.createModelAndView('sipstack', {
-      sipstack: require('webrtc-sipstack')
+      sipstack: require('webrtc-sipstack'),
+      eventbus: require('bdsft-sdk-eventbus'),
+      debug: require('bdsft-sdk-debug'),
+      core: require('webrtc-core')
     });
     test.createModelAndView('settings', {
       settings: require('../'),
